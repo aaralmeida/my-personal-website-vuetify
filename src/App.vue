c39c53c00467f3e70497f6b39164cd6e9489c13b
Armando <armandoalanalmeida@gmail.com>
2017-12-03 22:53:54 -0300

    first Commit personal website with vuetify

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer absolute temporary v-model="sideNave">
+      <v-list>
+        <v-list-tile v-for="item in menuItens" :key="item.title">
+          <v-list-tile-action>
+            <v-icon>{{item.icons}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{item.title}}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar cliped-left dark class="primary" fixed app>
+      <v-toolbar-side-icon @click.native.stop="sideNave= !sideNave" class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>Armando A.</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat v-for="item in menuItens" :key="item.title">
+          <v-icon left>{{item.icons}}</v-icon>
+          {{item.title}}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+    
+    <v-footer  app>
+      <span class="text-lg-center">&copy; Armando Almeida 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      sideNave: false,
+      menuItens: [
+        { icons: 'account_circle', title: 'About me' },
+        { icons: 'work', title: 'Portifolio' },
+        { icons: 'build', title: 'Skills' },
+        { icons: 'contacts', title: 'Contact me' }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <v-avatar size="250px" class="mt-5">
+          <img src="/static/img/perfil.jpg" alt="Vuetify.js" class="mb-5">
+        </v-avatar>
+        <p>Sou um programador apaixonado</p>
+        <blockquote>
+          &#8220;First, solve the problem. Then, write the code.&#8221;
+          <footer>
+            <small>
+              <em>&mdash;John Johnson</em>
+            </small>
+          </footer>
+        </blockquote>
+      </v-layout>
+    </v-slide-y-transition>
+    <v-divider></v-divider>
+    <section>
+      <v-layout>
+        <v-flex text-xs-center>
+          <v-avatar>
+            
+          </v-avatar>
+        </v-flex>
+      </v-layout>
+    </section>
+  </v-container>
+</template>
